42733479fb30c55f49990fbe4f29b42ccf04c7b5
gomeliGithub <irina01041971@mail.ru>
2024-05-19 00:31:30 +0300

    Main Commit

--- backend/src/pydanticClasses/signData.py
+++ backend/src/pydanticClasses/signData.py
@@ -4,7 +4,3 @@ from pydantic import BaseModel, Field
 class SignIn_request_pydantic (BaseModel):
     login: str = Field(min_length = 4)
     password: str = Field(min_length = 5)
-
-
-class SignIn_response_pydantic (BaseModel):
-    pass
