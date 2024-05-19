b9c318cb2ac660a05acc74638aa50e655fc245f7
gomeliGithub <irina01041971@mail.ru>
2024-05-19 22:29:55 +0300

    Main Commit

--- backend/src/pydanticClasses/examData.py
+++ backend/src/pydanticClasses/examData.py
@@ -4,6 +4,7 @@ from datetime import (
     timedelta
 )
 
+from fastapi import Query
 from pydantic import (
     BaseModel, 
     Field,
@@ -12,17 +13,17 @@ from pydantic import (
 
 
 class Exam_get_request_pydantic (BaseModel):
-    studySubjectId: int | None = Field(default = None, ge = 0)
-    isConsultation: bool | None = Field(default = None)
+    studySubjectId: int | None = Field(Query(default = None, ge = 0))
+    isConsultation: bool | None = Field(Query(default = None))
 
 
 class Department_get_request_pydantic (BaseModel):
-    studentsAreShortage: bool | None = Field(default = None)
+    studentsAreShortage: bool | None = Field(Query(default = None))
 
 
 class Specialty_get_request_pydantic (BaseModel):
-    includePassingScore: bool = Field(default = True)
-    includeCompetition: bool = Field(default = True)
+    includePassingScore: bool = Field(Query(default = True))
+    includeCompetition: bool = Field(Query(default = True))
 
 
 class Exam_create_request_pydantic (BaseModel):
--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -5,12 +5,11 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 import { AuthInterceptor } from './interceptors/auth/auth.interceptor';
-// import { ErrorInterceptor } from './interceptors/error/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter(routes),
         importProvidersFrom(HttpClientModule),
-        provideHttpClient(withFetch(), withInterceptors([ AuthInterceptor ])) // ErrorInterceptor
+        provideHttpClient(withFetch(), withInterceptors([ AuthInterceptor ]))
     ]
 };
