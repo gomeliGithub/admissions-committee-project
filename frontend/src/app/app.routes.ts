07079dd1c85de8dd16c7087298171f9385b4a022
gomeliGithub <irina01041971@mail.ru>
2024-05-20 00:08:38 +0300

    Main Commit

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,11 +5,9 @@ import { MainComponent } from './components/main/main.component';
 import { SignGuard } from './guards/sign/sign.guard';
 
 import { SignComponent } from './components/sign/sign.component';
-import { ForbiddenComponent } from './components/forbidden/forbidden.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/main', pathMatch: 'full' },
     { path: 'main', component: MainComponent, canActivate: [SignGuard] },
-    { path: 'sign/in', component: SignComponent },
-    { path: 'forbidden', component: ForbiddenComponent }
+    { path: 'sign/in', component: SignComponent }
 ];
